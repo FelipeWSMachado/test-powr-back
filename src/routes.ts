b885c7e9003f77b45bf4cc4708bdb5952322c61e
Felipe Machado <47465519+FelipeWSMachado@users.noreply.github.com>
2022-01-29 18:50:20 -0300

    Procfile ajusts

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,6 @@ const express = require("express");
 import RegisterJson from "./Controllers/registerJSON";
 const routes = express.Router();
 routes.use(express.json());
-routes.get("/", (req: any, res: any) => {
-  res.send("Hello");
-});
 
 routes.post("/", RegisterJson.registeItem);
 routes.get("/:id", RegisterJson.getItensByID);
